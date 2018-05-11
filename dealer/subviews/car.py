afd6d55e0e8e40561eebb066eb2fb42ba57e1815
ryo <ryo.hang@gmail.com>
2018-05-11 16:18:25 -0400

    use generic view and mixin

--- dealer/subviews/car.py
+++ dealer/subviews/car.py
@@ -1,14 +1,21 @@
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework.response import Response
 from dealer.models import Car
 from dealer.serializers import CarSerializer
 
-class CarList(APIView):
+class Car(mixins.ListModelMixin,generics.GenericAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-    def get(self, request, format=None):
-        cars = Car.objects.all()
-        serializer = CarSerializer(cars, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+    # def get(self, request, format=None):
+    #     cars = Car.objects.all()
+    #     serializer = CarSerializer(cars, many=True)
+    #     return Response(serializer.data)
 
     # def post(self, request, format=None):
     #     serializer = SnippetSerializer(data=request.data)
--- dealer/urls.py
+++ dealer/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CarList
+from .views import Car
 
 urlpatterns = [
-    url(r'^cars$', CarList.as_view()),
+    url(r'^cars$', Car.as_view()),
     # url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
